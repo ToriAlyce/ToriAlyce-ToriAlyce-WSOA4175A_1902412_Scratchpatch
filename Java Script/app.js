1f57bda1ae0711daad1dcacc6558d90f85aafe58
ToriAlyce <60913663+ToriAlyce@users.noreply.github.com>
2021-10-02 16:40:59 +0200

    scratch
    added scratch feature

---
+++ Java Script/app.js
@@ -0,0 +1,7 @@
+
+$('#card').wScratchPad({
+    size: 100,          // The size of the brush/scratch.
+    bg : './Images/ticket_cover_layer.png',  // Background (image path or hex color).
+    fg : './Images/ticket_cover_homepage.png',  // Foreground (image path or hex color).
+    cursor      : 'pointer' // Set cursor.
+  });
